e0caae63817b57b884895153e790af3bafdee1c8
FisherCloud <fishercloud@qq.com>
2020-03-16 12:52:02 +0800

    little modify

--- momo-share.py
+++ momo-share.py
@@ -10,7 +10,7 @@ from termcolor import colored
 
 
 class momo_share:
-    def __init__(self, url, TargetNum=25, proxynum=120):
+    def __init__(self, url='', TargetNum=20, proxynum=100):
         super().__init__()
         self.header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'}
@@ -25,7 +25,7 @@ class momo_share:
     def getProxy(self):
         if self.completion >= self.TargetNum:
             return 0
-        print('[+] %s' % colored('get proxy...', 'blue', attrs=['bold']), end='')
+        # print('[+] %s' % colored('get proxy...', 'blue', attrs=['bold']), end='')
 
         while 1:
             try:
@@ -37,16 +37,15 @@ class momo_share:
                     r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]+", html)) - set(self.ProxyList)
 
                 if not len(self.proxies):
-                    print('\n  [-]Waiting')
-                    time.sleep(random(0, 3))
+                    # print('\n  [-]Waiting')
+                    time.sleep(1)
                     continue
 
-                print(colored('[%d]' % len(self.proxies), 'yellow', attrs=['bold']), '%s' % (
-                    colored('Done!', 'green', attrs=['bold'])))
+                # print(colored('[%d]' % len(self.proxies), 'yellow', attrs=['bold']), '%s' % (colored('Done!', 'green', attrs=['bold'])))
                 return 1
             except Exception as e:
-                print('\n  [-]Error: ' + str(e))
-                time.sleep(random(1, 6))
+                # print('\n  [-]Error: ' + str(e))
+                time.sleep(random(1, 3))
                 pass
 
     async def autoVisit(self, proxy, sem):
@@ -54,8 +53,8 @@ class momo_share:
             async with aiohttp.ClientSession() as session:
                 try:
                     async with session.get(url=url, proxy='http://' + proxy, timeout=5) as resp:
-                        print('[%s]' % colored(proxy, 'cyan', attrs=['bold']),
-                              colored('Successfully!', 'green', attrs=['bold']), '[%s]' % colored(self.completion+1, 'blue', attrs=['bold']))
+                        # print('[%s]' % colored(proxy, 'cyan', attrs=['bold']), colored('Successfully!', 'green', attrs=['bold']), '[%s]' % colored(self.completion+1, 'blue', attrs=['bold']))
+                        pass
                     self.ProxyList.append(proxy)
                     self.completion += 1
                 except Exception as e:
@@ -71,11 +70,14 @@ class momo_share:
                     self.autoVisit(i, sem)) for i in self.proxies]
                 loop.run_until_complete(asyncio.wait(tasks))
             loop.close()
-            print(colored(self.completion, 'yellow',
-                          attrs=['bold']))
-        else:
-            print('error')
+            # print(colored(self.completion, 'yellow', attrs=['bold']))
             pass
+        else:
+            # print('error')
+            pass
+
+    def getCompletionNumber(self):
+        return self.completion
 
 
 if __name__ == "__main__":
@@ -83,24 +85,34 @@ if __name__ == "__main__":
     # get in one request of free proxy site
     # proxynum = 100
     # how many visition your want to get, great or equal TargetNum what you set
-    # TargetNum = 25
+    # TargetNum = 20
 
-    # get argv
-    from sys import argv
-    print(argv)
-    if 4 == argv.__len__():
-        filename, url, proxynum, targetnum = argv
-        momo = momo_share(url=url, TargetNum=int(
-            targetnum), proxynum=int(proxynum))
-    elif 3 == argv.__len__():
-        filename, url, targetnum = argv
-        momo = momo_share(url=url, TargetNum=int(targetnum))
-    elif 2 == argv.__len__():
-        filename, url = argv
-        momo = momo_share(url)
+    try:
+        # get argv
+        from sys import argv
+        print(argv)
+        if 4 == argv.__len__():
+            filename, url, proxynum, targetnum = argv
+            momo = momo_share(url=url, TargetNum=int(
+                targetnum), proxynum=int(proxynum))
+        elif 3 == argv.__len__():
+            filename, url, targetnum = argv
+            momo = momo_share(url=url, TargetNum=int(targetnum))
+        elif 2 == argv.__len__():
+            filename, url = argv
+            momo = momo_share(url)
+        else:
+            print(
+                "python -u momo-share.py url [targetnum] [proxynum]\ndefault: targetnum = 20, proxynum = 100")
+            sys.exit(0)
+
+        # run
+        momo.run()
+        print("success:[%d]" % momo.getCompletionNumber())
+        pass
+    except KeyboardInterrupt:
+        # print("exit")
+        pass
     else:
-        print("python -u momo-share.py 'url' 'targetnum' 'proxynum'")
-        sys.exit(0)
-
-    # run
-    momo.run()
+        # print("success")
+        pass
